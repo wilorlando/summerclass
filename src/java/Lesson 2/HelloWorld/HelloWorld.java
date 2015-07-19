3a6e93e82a7a47d212948bc8f65d4667cbfa3f56
Wil Orlando <wilorlando@yahoo.com>
2015-07-19 16:46:53 -0400

    The final Hello World edit

--- src/java/Lesson 2/HelloWorld/HelloWorld.java
+++ src/java/Lesson 2/HelloWorld/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello, Sam!");
+        System.out.println("Hello, Wil!");
     } //End main
 } // End HelloWorld
